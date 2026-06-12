e171ce0df7d6dded55bc45cd5f7d08d64c98ab4a
ManuelMtz1018 <jmanuel1018mtz@gmail.com>
2026-06-12 16:17:14 -0600

    Model update

--- src/main/java/org/example/ledsdbsatus/models/Lamp.java
+++ src/main/java/org/example/ledsdbsatus/models/Lamp.java
@@ -1,9 +1,13 @@
 package org.example.ledsdbsatus.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.example.ledsdbsatus.exceptions.validations.IsRequired;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "lamps_status")
 public class Lamp {
     @Id
@@ -14,45 +18,5 @@ public class Lamp {
     private boolean status;
     private String description;
 
-    public Lamp() {}
-
-    public Lamp(Long id, String label, boolean status, String description) {
-        this.id = id;
-        this.label = label;
-        this.status = status;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
 
